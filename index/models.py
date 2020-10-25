1e5a1205f55dbf6e4f52e1aa0bc3481c97372ac7
fax0nat <1_0_91@bk.ru>
2020-10-25 18:15:45 +0300

    Added some models

--- index/models.py
+++ index/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    CASH = 'Cash'
+    CARD = 'Card'
+    METHOD_PAYMENT = [
+        (CASH, 'Cash'),
+        (CARD, 'Card')
+    ]
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    count = models.IntegerField()
+#   user_id = models.ForeignKey('User', on_delete=models.CASCADE())
+    address_id = models.ForeignKey('Address', on_delete=models.CASCADE)
+    payment_method = models.CharField(choices=METHOD_PAYMENT, max_length=50)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=100)
+    street = models.TextField()
+
+
+class CreditCard(models.Model):
+    CVV = models.IntegerField()
+    number = models.IntegerField()
+    card_holder = models.CharField(max_length=100)
+    exp_date = models.DateField()
+#   user_id = models.ForeignKey('User', on_delete=models.CASCADE())S
+
+
