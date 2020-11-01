8138f3d98ef5a9ac9c5f748c8464d793e801196d
DimaBakanov <dima.bakanov@mail.ru>
2020-11-01 17:22:10 +0300

    • Созданы необходимые модели • Создан базовый .js файл. Подключён jQuery

--- index/models.py
+++ index/models.py
@@ -1,39 +1,6 @@
 from django.db import models
 
 
-class Item(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.FloatField()
-
-
-class Order(models.Model):
-    CASH = 'Cash'
-    CARD = 'Card'
-    METHOD_PAYMENT = [
-        (CASH, 'Cash'),
-        (CARD, 'Card')
-    ]
-    item = models.ForeignKey('Item', on_delete=models.CASCADE)
-    count = models.IntegerField()
-#   user_id = models.ForeignKey('User', on_delete=models.CASCADE())
-    address_id = models.ForeignKey('Address', on_delete=models.CASCADE)
-    payment_method = models.CharField(choices=METHOD_PAYMENT, max_length=50)
-
-
-class Address(models.Model):
-    city = models.CharField(max_length=100)
-    street = models.TextField()
-
-
-class CreditCard(models.Model):
-    CVV = models.IntegerField()
-    number = models.IntegerField()
-    card_holder = models.CharField(max_length=100)
-    exp_date = models.DateField()
-#   user_id = models.ForeignKey('User', on_delete=models.CASCADE())S
-
-
-
 class CustomUser(models.Model):
     login = models.CharField(max_length=50, verbose_name='Имя пользователя в системе (никнейм)')
     password = models.CharField(max_length=30, verbose_name='Пароль пользователя')
@@ -70,3 +37,35 @@ class UserCard(models.Model):
     class Meta:
         verbose_name_plural = 'Медицинские карты пользователей'
         verbose_name = 'Медицинская карта пользователя'
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    CASH = 'Cash'
+    CARD = 'Card'
+    METHOD_PAYMENT = [
+        (CASH, 'Cash'),
+        (CARD, 'Card')
+    ]
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    count = models.IntegerField()
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, default=None)
+    address = models.ForeignKey('Address', on_delete=models.CASCADE)
+    payment_method = models.CharField(choices=METHOD_PAYMENT, max_length=50)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=100)
+    street = models.TextField()
+
+
+class CreditCard(models.Model):
+    CVV = models.IntegerField()
+    number = models.IntegerField()
+    card_holder = models.CharField(max_length=100)
+    exp_date = models.DateField()
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, default=None)
