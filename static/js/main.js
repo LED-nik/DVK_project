e29275faebd02c0e30d297e81e3cfa1e73a83751
DimaBakanov <dima.bakanov@mail.ru>
2020-11-08 21:23:07 +0300

    Интегрировал фронт Авторизации

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
+$(document).ready(function () {
+    $('.header').height($(window).height());
+
+    $('#loginForm').on('submit', () => {
+     let login = $('#loginInput').val(),
+         password = $('#passwordInput').val();
+     alert(`your login is ${login}. And password is ${password}`);
+    })
 
 
-})
+});
