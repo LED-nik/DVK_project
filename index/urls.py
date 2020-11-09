0b03fbeda2ead2ee9d065475857d428db14fb73d
DimaBakanov <dima.bakanov@mail.ru>
2020-11-09 22:11:50 +0300

    Интегрирован чат

--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LogInView, UserCreateView, MainPageView, LogOutView
+from .views import LogInView, UserCreateView, MainPageView, LogOutView, ChatView
 
 urlpatterns = [
     path('', LogInView.as_view()),
     path('create_user', UserCreateView.as_view()),
     path('welcome', MainPageView.as_view()),
     path('log_out', LogOutView.as_view()),
+    path('send_message', ChatView.as_view()),
 ]
